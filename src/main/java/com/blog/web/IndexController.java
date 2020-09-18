7f11dd566f06d4e3963f422ad665d49bba1fb7f0
Goddran <1104015065@qq.com>
2020-09-18 09:17:17 +0800

    Initial commit

---
+++ src/main/java/com/blog/web/IndexController.java
@@ -0,0 +1,33 @@
+package com.blog.web;
+
+
+import com.blog.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+////        int i = 9/0;
+//        String blog = null;
+//        if(blog ==null){
+//            throw new NotFoundException();
+//        }
+//        System.out.println("---------index---------");
+        return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+////        int i = 9/0;
+//        String blog = null;
+//        if(blog ==null){
+//            throw new NotFoundException();
+//        }
+//        System.out.println("---------index---------");
+        return "blog";
+    }
+}
