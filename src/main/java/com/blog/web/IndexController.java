170cb50c9219d13f5c51cd43c37b8099110177f6
Goddran <1104015065@qq.com>
2020-11-29 21:40:45 +0800

    着手处理图片上传问题之前做一次版本记录
    Signed-off-by: Goddran <1104015065@qq.com>

--- src/main/java/com/blog/web/IndexController.java
+++ src/main/java/com/blog/web/IndexController.java
@@ -1,33 +1,53 @@
 package com.blog.web;
 
 
-import com.blog.NotFoundException;
+import com.blog.service.BlogService;
+import com.blog.service.TagService;
+import com.blog.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private BlogService blogService;
+
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private TypeService typeService;
+
     @GetMapping("/")
-    public String index(){
-////        int i = 9/0;
-//        String blog = null;
-//        if(blog ==null){
-//            throw new NotFoundException();
-//        }
-//        System.out.println("---------index---------");
+    public String index(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                        Model model) {
+        model.addAttribute("page", blogService.listBlog(pageable));
+        model.addAttribute("types", typeService.listTypeTop(6));
+        model.addAttribute("tags", tagService.listTag(10));
+        model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
         return "index";
     }
 
-    @GetMapping("/blog")
-    public String blog(){
-////        int i = 9/0;
-//        String blog = null;
-//        if(blog ==null){
-//            throw new NotFoundException();
-//        }
-//        System.out.println("---------index---------");
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, Model model) {
+        model.addAttribute("page", blogService.listBlog("%" + query + "%", pageable));
+        model.addAttribute("query", query);
+        return "search";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blog(@PathVariable Long id, Model model) {
+        model.addAttribute("blog", blogService.getAndConvert(id));
         return "blog";
     }
 }
